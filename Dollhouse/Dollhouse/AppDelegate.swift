e0027f26c7e245e248380c492973944a3fe9b7ee
Dulio Denis <dulio.denis@yahoo.com>
2015-04-11 19:40:47 -0400

    Initial commit

---
+++ Dollhouse/Dollhouse/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Dollhouse
+//
+//  Created by Dulio Denis on 4/11/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bolts
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Power the app with Local Datastore
+        Parse.enableLocalDatastore()
+        // Initialize Parse.
+        Parse.setApplicationId("YOUR_APP_ID",
+            clientKey: "YOUR_CLIENT_ID")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+
+        return true
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dollhouse
+# Dollhouse
 Keep in touch with your girlfriends and all their gossip with this delightful iOS chat app written in Swift that uses Parse.
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
