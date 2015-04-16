39eb5c37cf7afa6da727fad7975daa418109b909
Dulio Denis <dulio.denis@yahoo.com>
2015-04-16 11:12:35 -0400

    Added loading/sending messages plus JSQMessageVC Delegate Methods

--- Dollhouse/Dollhouse/MessagesViewController.swift
+++ Dollhouse/Dollhouse/MessagesViewController.swift
@@ -16,7 +16,7 @@ class MessagesViewController: JSQMessagesViewController {
     var users = [PFUser]()
     
     var messages = [JSQMessage]()
-    var messageObject = [PFObject]()
+    var messageObjects = [PFObject]()
     
     var outgoingBubbleImage = JSQMessagesBubbleImage()
     var incomingBubbleImage = JSQMessagesBubbleImage()
@@ -24,6 +24,9 @@ class MessagesViewController: JSQMessagesViewController {
     var selfAvatar = JSQMessagesAvatarImage()
     var incomingAvatar = JSQMessagesAvatarImage()
 
+    
+    // MARK: - View Controller Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -47,23 +50,146 @@ class MessagesViewController: JSQMessagesViewController {
         let bubbleFactory = JSQMessagesBubbleImageFactory()
         outgoingBubbleImage = bubbleFactory.outgoingMessagesBubbleImageWithColor(UIColor.lightGrayColor())
         incomingBubbleImage = bubbleFactory.incomingMessagesBubbleImageWithColor(UIColor.grayColor())
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Load Messages
+    
+    func loadMessages() {
+        var lastMessage:JSQMessage? = nil
+        
+        if messages.last != nil {
+            lastMessage = messages.last
+        }
+        
+        let messageQuery = PFQuery(className: "Message")
+        messageQuery.whereKey("room", equalTo: room)
+        messageQuery.limit = 500
+        messageQuery.includeKey("user")
+        
+        // only load the newest messages
+        if lastMessage != nil {
+            messageQuery.whereKey("createdAt", greaterThan: lastMessage!.date)
+        }
+        
+        messageQuery.findObjectsInBackgroundWithBlock { (results: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                let messages = results as! [PFObject]
+                
+                // for every message in the messages array
+                for message in messages {
+                    // Add the message to the messages object
+                    self.messageObjects.append(message)
+                    
+                    // then add the user
+                    let user = message["user"] as! PFUser
+                    self.users.append(user)
+                    
+                    // and add the message to the JSQMessages Array
+                    let chatMessage = JSQMessage(senderId: user.objectId, senderDisplayName: user.username, date: message.createdAt, text: message["content"] as! String)
+                    self.messages.append(chatMessage)
+                }
+                
+                if results!.count != 0 {
+                    self.finishReceivingMessage()
+                }
+            }
+        }
     }
-    */
+    
+    
+    // MARK: - Send Messages
+    
+    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        let message = PFObject(className: "Message")
+        message["content"] = text
+        message["room"] = room
+        message["user"] = PFUser.currentUser()
+        
+        message.saveInBackgroundWithBlock { (success: Bool, error:NSError?) -> Void in
+            if error == nil {
+                self.loadMessages()
+                
+                self.room["lastUpdated"] = NSDate()
+                self.room.saveInBackgroundWithBlock(nil)
+            } else {
+                println("Error sending message: \(error?.localizedDescription)")
+            }
+        }
+        self.finishSendingMessage()
+    }
+    
+    
+    // MARK: - JSQMessageVC Delegate Methods
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.row]
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.row]
+        
+        if message.senderId == self.senderId {
+            return outgoingBubbleImage
+        }
+        
+        return incomingBubbleImage
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        let message = messages[indexPath.row]
+        
+        if message.senderId == self.senderId {
+            return selfAvatar
+        }
+        
+        return incomingAvatar
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForCellTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
+        if indexPath.item % 3 == 0 {
+            let message = messages[indexPath.item]
+            
+            return JSQMessagesTimestampFormatter.sharedFormatter().attributedTimestampForDate(message.date)
+        }
+        
+        return nil
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
+        
+        let message = messages[indexPath.row]
+        
+        if message.senderId == self.senderId {
+            cell.textView.textColor = UIColor.blackColor()
+        } else {
+            cell.textView.textColor = UIColor.whiteColor()
+        }
+        
+        cell.textView.linkTextAttributes = [NSForegroundColorAttributeName:cell.textView.textColor]
+        
+        return cell
+    }
+    
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        if indexPath.item % 3 == 0 {
+            return kJSQMessagesCollectionViewCellLabelHeightDefault
+        }
+        
+        return 0
+    }
+    
+    
+    // MARK: - Data Source
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
 
 }
