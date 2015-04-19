b4d0d04955ce7a8a7ac1403cad15ba760f0e1629
Dulio Denis <dulio.denis@yahoo.com>
2015-04-19 17:10:53 -0400

    Added Bolts framework to the Bridging Header

--- Dollhouse/Dollhouse/Dollhouse-Bridging-Header.h
+++ Dollhouse/Dollhouse/Dollhouse-Bridging-Header.h
@@ -2,6 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#import <Parse/Parse.h>
+#import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
 #import "JSQMessages.h"
--- Dollhouse/Dollhouse/LoginSignUpViewController.swift
+++ Dollhouse/Dollhouse/LoginSignUpViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Parse
 
 class LoginSignUpViewController: PFLogInViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
     
